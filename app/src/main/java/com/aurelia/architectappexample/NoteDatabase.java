ea5062c787a8f88c9266f5d5fdb58cd1ce16ff06
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-05-28 07:19:15 -0700

    Created NoteRepository class and DB population methods. Updated NoteDatabase class with callbacks and async task functionality.

--- app/src/main/java/com/aurelia/architectappexample/NoteDatabase.java
+++ app/src/main/java/com/aurelia/architectappexample/NoteDatabase.java
@@ -1,25 +1,56 @@
 package com.aurelia.architectappexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 
     private static NoteDatabase instance;
 
+    private static final RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
     public static synchronized NoteDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
-                    NoteDatabase.class, "note_database").fallbackToDestructiveMigration().build();
+                    NoteDatabase.class, "note_database").fallbackToDestructiveMigration().addCallback(roomCallback).build();
         }
 
         return instance;
     }
 
     public abstract NoteDao noteDao();
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private final NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Tit1", "Desc1", 1));
+            noteDao.insert(new Note("Tit2", "Desc3", 2));
+            noteDao.insert(new Note("Tit3", "Desc2", 3));
+
+
+            return null;
+        }
+    }
+
 
 }
