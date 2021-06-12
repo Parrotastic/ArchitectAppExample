5ffda634f656cf8c22a09bb9eb210573d6a20f60
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-06-12 13:49:19 -0700

    Added Observer for LiveData and beginning working on RecyclerView.

--- app/src/main/java/com/aurelia/architectappexample/MainActivity.java
+++ app/src/main/java/com/aurelia/architectappexample/MainActivity.java
@@ -1,8 +1,13 @@
 package com.aurelia.architectappexample;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private NoteViewModel noteViewModel;
@@ -13,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //noteViewModel =
+        noteViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onchanged", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
