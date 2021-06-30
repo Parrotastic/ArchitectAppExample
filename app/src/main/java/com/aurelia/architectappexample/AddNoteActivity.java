c5a799590c38c7b8529c528c719801988c851741
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-06-30 16:36:33 -0700

    Created AddNoteActivity and corresponding linear layout file.

---
+++ app/src/main/java/com/aurelia/architectappexample/AddNoteActivity.java
@@ -0,0 +1,14 @@
+package com.aurelia.architectappexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
--- app/src/main/java/com/aurelia/architectappexample/MainActivity.java
+++ app/src/main/java/com/aurelia/architectappexample/MainActivity.java
@@ -1,11 +1,12 @@
 package com.aurelia.architectappexample;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -18,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
+
         noteViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                //update RecyclerView
-                Toast.makeText(MainActivity.this, "onchanged", Toast.LENGTH_LONG).show();
+
+                adapter.setNotes(notes);
             }
         });
     }
